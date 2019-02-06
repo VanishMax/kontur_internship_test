20b567989bfc1d6bd0d67703e95b4f037abe9c1c
VanishMax <maxkors2014@gmail.com>
2019-02-06 16:54:53 +0300

    Format is fully right now

--- parseTodo.js
+++ parseTodo.js
@@ -1,16 +1,26 @@
 parseTodo = (files) => {
   var regexp = /\/\/ ?todo ?.*?\n/gi;
   return files.map( (file) => {
-    file.content = file.content.match(regexp);
+    file.content = file.content.match(regexp) || [];
     file.content = file.content.map( (todo) => {
       let important = /!/.test(todo);
       let todoSplitted = todo.split(/;/);
+
       let user = todoSplitted[0].match(/todo ?(.*)/i)[1].trim();
-      let date = todoSplitted[1].trim();
-      let comment = todoSplitted[todoSplitted.length - 1].match(/ ?(.*)\n/i)[1].trim();
-      if(comment.length >= 40){
-        comment = comment.substr(0, 37) + '...'
+      if(user.length > 10){
+        user = user.substr(0, 7) + '...'
       }
+
+      let date = todoSplitted[1].trim();
+      if(date.length > 10){
+        date = date.substr(0, 7) + '...'
+      }
+
+      let comment = todoSplitted[todoSplitted.length - 1].match(/ ?(.*)\n/i)[1].trim();
+      if(comment.length > 50){
+        comment = comment.substr(0, 47) + '...'
+      }
+
       return {
         full: todo,
         important: important,
@@ -19,6 +29,11 @@ parseTodo = (files) => {
         comment: comment
       };
     });
+
+
+    if(file.name.length > 15){
+      file.name = file.name.substr(0, 12) + '...'
+    }
     return file;
   });
 };
@@ -26,12 +41,13 @@ parseTodo = (files) => {
 printTodo = (files) => {
 
   // What is the length of the cells of the printed table
-  let counts = { user: 0, date: 0, comment: 0, name: 0 };
+  let counts = { user: 4, date: 4, comment: 7, name: 4, todos: 0 };
 
   // Count the length
   files.forEach( (file) => {
     if(file.name.length > counts.name) counts.name = file.name.length;
     file.content.forEach( (todo) => {
+      counts.todos++;
       if(todo.user.length > counts.user) counts.user = todo.user.length;
       if(todo.date.length > counts.date) counts.date = todo.date.length;
       if(todo.comment.length > counts.comment) counts.comment = todo.comment.length;
@@ -58,7 +74,7 @@ printTodo = (files) => {
       printTodos.push(' '.repeat(2) + file.name + ' '.repeat(counts.name - file.name.length + 2) + '\n');
     });
   });
-  printTodos.push('-'.repeat(counts.user + counts.name + counts.date + counts.comment + 25) + '\n');
+  if(counts.todos !== 0) printTodos.push('-'.repeat(counts.user + counts.name + counts.date + counts.comment + 25) + '\n');
 
   return printTodos.join('');
 };
